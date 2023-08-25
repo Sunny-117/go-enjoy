ba4720b98816818ba725966f8f13d14017f557ce
fuzhiqiang <fuzhiqiang@baidu.com>
2023-08-25 21:09:23 +0800

    chore: getsumandsub

--- main.go
+++ main.go
@@ -1,5 +1,14 @@
 package main
 
+import "fmt"
+
+func getSumAndSub(n1 int, n2 int) (int, int) {
+	sum := n1 + n2
+	sub := n1 - n2
+	return sum, sub
+}
 func main() {
 	// coding
+	var a, b = getSumAndSub(3, 2)
+	fmt.Println(a, b)
 }
